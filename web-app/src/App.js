e78494049067cbfe2f2bd8d3b2372c21e9d74479
James <kamaujames3705@gmail.com>
2023-05-02 15:03:12 +0300

    implemented navbar

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -4,6 +4,7 @@ import Header from './Components/Header';
 function App() {
   return (
  <div>
+  
   <Header />
  </div>
     
--- web-app/src/Components/Header.js
+++ web-app/src/Components/Header.js
@@ -2,24 +2,31 @@ import React from 'react'
 
 function Header() {
   return (
+    <div>
     <div className='head'>
+        <nav className='nav'>
         <h1 id = 'h1'>Hope Funding Organisation</h1>
         <div class="container">
         <button class="btn">Sign In</button>
         <button class="btn">Log In</button>
-    </div>
-    
+        </div>
+        </nav>
+        
+        
+        
     <article id='art'>
         Hope Crowd Funding Campaigns
     </article>
-    <p id='txt'>
+    <p className='txt'>
     We’ve helped people from all over Kenya crowdfund <br></br>
     the things that matter to them most. From personal fundraisers, <br></br>
     to acts of kindness, emergency relief, and more. <br></br>
     Create your campaign for free!
     </p>
-      
-    </div>
+    <img id='pic'src='https://shorturl.at/EGP45' alt=''/>
+      </div>
+    
+      </div>
   )
 }
 
